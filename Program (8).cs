2e8f4210952b53f2e3da3882aac0b10ba3579d4c
AMBER AHMED <77809281+GrooveToMyTensorFlow@users.noreply.github.com>
2023-05-04 02:51:30 -0400

    Add files via upload

---
+++ Program (8).cs
@@ -0,0 +1,506 @@
+﻿// 0719581 - Dharamraj Patel
+// 0680481 – Amber Ahmed
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3020Assignment1
+{
+   
+
+    public enum Colour { EMPTY, RED, YELLOW, GREEN, BLUE,GREY}
+
+    public class SubwayMap
+    {
+
+        private class Node
+        {
+            public Station Connection; // Adjacent station (connection)
+            public Colour Line;  // Colour of its subway line
+            public Node Next;     // Link to the next adjacent station (Node)
+
+
+
+            //
+            public Node()
+            {
+                Next = null; //
+                Line = Colour.EMPTY;
+                Connection = null;
+            }
+
+
+            public Node(Station connection, Colour c, Node next)
+            {
+                Connection = connection;
+                Line = c;
+                Next = next;
+            }
+
+        }
+        //---------------------------------------------------------------------
+        private class Station
+        {
+            public string Name;      // Name of the subway station
+            public bool visited;     // Used for the breadth-first search
+            public Node E;           // Header node for a linked list of adjacent stations
+
+            public Station Parent;
+
+            public int Count;      // Number of Adjecent stations
+
+            public Station(string name)
+            {
+                Name = name;
+                visited = false;
+                Count = 0;
+                E = new Node();
+                Parent = null;
+
+            }
+        }
+
+        //---------------------------------------------------------------------
+
+        // Class data members
+
+        private Dictionary<string, Station> S;          //Dictionary of stations
+
+        public SubwayMap()
+        {
+            S = new Dictionary<string, Station>();
+
+        }
+
+        //---------------------------------------------------------------------
+
+        public void InsertStation(string name)
+        {
+            if (!(S.ContainsKey(name)))
+            {
+                S.Add(name, new Station(name));
+            }
+        }
+
+        //---------------------------------------------------------------------
+        public bool RemoveStation(string name)
+        {
+            if (S.ContainsKey(name))
+            {
+                Node curr = S[name].E;
+
+                // if the staion has more that one one connection
+                // delete the connection from the adjecent stations and then delete the Key
+                while (S[name].Count > 0)
+                {
+                    curr = curr.Next;
+                    StationRemove(curr.Connection.Name, name);
+                    S[name].Count -= 1;
+                    
+                }
+                S.Remove(name);
+                Console.WriteLine("Station '" + name + "' removed from the map");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Station not found");
+                return false;
+            }
+
+        }
+
+        public void StationRemove(string adj, string S_name)
+        {
+            bool found = false;
+            Node newCurr = S[adj].E;
+            while (newCurr.Next != null && !found)
+            {
+
+                if (newCurr.Next.Connection.Name.Equals(S_name))
+                {
+                    newCurr.Next = newCurr.Next.Next;
+                    S[adj].Count--;
+                    found = true;
+
+                }
+                else
+                {
+                    newCurr = newCurr.Next;
+                }
+            }
+
+        }
+
+
+        //---------------------------------------------------------------------
+
+        public bool InsertConnection(string name1, string name2, Colour c)
+        {
+            // check if both stations exist
+            if (S.ContainsKey(name1) && S.ContainsKey(name2))
+            {
+                Node curr = S[name1].E;
+                Node front = S[name2].E;
+
+                // if there are no adjecent connections to that station yet.
+                if (S[name1].Count == 0)
+                {
+                    curr.Next = new Node(S[name2], c, curr.Next);
+                    front.Next = new Node(S[name1], c, front.Next);
+                    S[name1].Count += 1;
+                    S[name2].Count += 1;
+                    //found = true;
+                    return true;
+                }
+
+                // if the station already has a few connections...
+                // We go through the linked list to see if there is already a connection.
+                int stationCount = 0;
+                while (curr.Next != null || stationCount < S[name1].Count)
+                {
+                    // if the connection already exists...
+                    if (curr.Next.Connection.Equals(S[name2]))
+                    {
+                        // if the connections exists on the same line
+                        if (curr.Next.Line.Equals(c))
+                        {
+                            Console.WriteLine("Connection already exists");
+                            return false;
+                        }
+                        // if there is a connection but not on the same line
+                        // Add a connection
+                        else
+                        {
+                            curr.Next = new Node(S[name2], c, curr.Next);
+                            front.Next = new Node(S[name1], c, front.Next);
+                            S[name1].Count += 1;
+                            S[name2].Count += 1;
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        stationCount++;
+                        curr = curr.Next;
+                    }
+                }
+
+
+                // if there is no connection between those 2 stations
+                curr.Next = new Node(S[name2], c, curr.Next);
+                front.Next = new Node(S[name1], c, front.Next);
+                S[name1].Count += 1;
+                S[name2].Count += 1;
+                //found = true;
+                return true;
+            }
+
+            return false; // 
+        }
+
+        public bool stationFound(string name)
+        {
+            foreach (var item in S)
+            {
+                if (item.Key == name)
+                {
+                    Console.WriteLine("Duplicate station");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //---------------------------------------------------------------------
+
+
+        public bool RemoveConnection(string name1, string name2, Colour c)
+        {
+            if (S.ContainsKey(name1) && S.ContainsKey(name2))
+            {
+                // if Connection A-B exists
+                // remove connection from station A to Station B
+                if (ConnectionRemove(name1, name2, c) == true)
+                {
+                    // then remove the other connection
+                    // remove connection from station B to Station A
+                    ConnectionRemove(name2, name1, c);
+                }
+                return true;   // return true when the connection is removed
+            }
+
+            return false;  // connection not removed
+        }
+
+
+        // visit all nodes of station st1 , to remove the node(station) that contaions st2
+        public bool ConnectionRemove(string st1, string st2, Colour C)
+        {
+
+            Node curr = S[st1].E;
+            bool found = false;
+
+            // go through the linked list 
+            while(curr.Next != null && found == false)
+            {
+                // if the station name equals st.name && the colour is the same
+                if(curr.Next.Connection.Name.Equals(st2) || curr.Next.Equals(C))
+                {
+                    curr.Next = curr.Next.Next; // remove that node
+                    found = true;               // break the loop
+                    
+                }
+                else
+                {
+                    curr = curr.Next;   // if node not found, go to the next node.
+                } 
+            }
+            // if !found > meaning the link(connection) was not removed/found
+            if (!found)
+            {
+                Console.WriteLine("No connection found");
+                return false;
+            }
+            else
+                return true; 
+                
+            
+        }
+
+        //---------------------------------------------------------------------
+
+
+        public void SRoute(string name1, string name2)
+        {
+            
+            if (S.ContainsKey(name1) && S.ContainsKey(name2))
+            {
+                Queue<Station> q = new Queue<Station>(); // QUEUE TO STORE THE STATIONS
+                
+
+                q.Enqueue(S[name1]); // DEPARTURE STATION
+
+                S[name1].visited = true; //DEPARTURE STATION , VISITED TRUE
+
+                bool sr = false;  // bool valu
+
+                while (q.Count != 0)
+                {
+                    Station s = q.Dequeue();  // pop the station
+
+                    if (s.Name == name2)   // 
+                    {
+                        Console.WriteLine(" Shortest route Found");
+                        sr = true;
+                        break;
+                    }
+
+                    Node curr = s.E; // SET THE NODE TO THE CURRENT STATION 
+
+                    // going through the list of the station (Node E)
+                    while (curr.Next != null)
+                    {
+                        if (!curr.Next.Connection.visited ) // if next station is not visited yet >  FALSE
+                        {
+                            q.Enqueue(curr.Next.Connection);       // push the next node to the queue
+                            curr.Next.Connection.visited = true;   // visited = true
+                            curr.Next.Connection.Parent = s;       // parent of the next node = current node
+                            
+                        }
+                        curr = curr.Next;
+                    }
+                }
+                // if shortest route is Found
+                // Traversing from the destination station to the departing station 
+                if (sr == true)  
+                {
+                    Station destination = S[name2];  // 
+                    
+
+                    // printing the stations in reverse ordder 
+                    while (destination.Parent != null)
+                    {
+                        Console.WriteLine( destination.Name); // the station we are currenly pointing to
+
+                        destination = destination.Parent;  // next parent station
+                        
+                    }
+                    Console.WriteLine(name1 + "  > Departing station "); // this is the departing station
+                }
+                else
+                {
+                    Console.WriteLine("Route not Found");
+                }
+
+
+                // changing back the values of all station to FALSE > NOT visited
+                foreach (Station nVis in S.Values)
+                {
+                    nVis.visited = false;
+                    nVis.Parent = null;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No route since one of the station is not found");
+            }
+        }
+   
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SubwayMap sm = new SubwayMap();
+
+            sm.InsertStation("MANCHESTER");
+            sm.InsertStation("CLIFTON");
+            sm.InsertStation("SHEFFIELD");
+            sm.InsertStation("LONDON");
+            sm.InsertStation("OSTERLY");
+            sm.InsertStation("HEATHROW");
+            sm.InsertStation("READING");
+            sm.InsertStation("SOUTHAMPTON");
+            sm.InsertStation("PORTSMOUTH");
+            sm.InsertStation("BRIGHTON");
+            sm.InsertStation("EDINBURGH");
+            sm.InsertStation("LEEDS");
+            sm.InsertStation("OXFORD");
+            sm.InsertStation("NEWBURY");
+            sm.InsertStation("WINCHESTER");
+            sm.InsertStation("MALBOROUGH");
+            sm.InsertStation("WARMINISTER");
+            sm.InsertStation("BOURMOUTH");
+            sm.InsertStation("HULL");
+            sm.InsertStation("COLCHESTER");
+            sm.InsertStation("BATH");
+            sm.InsertStation("BRISTOL");
+            sm.InsertStation("CARDIFF");
+            sm.InsertStation("PETERBOROUGH");
+            sm.InsertStation("IPSWICH");
+            sm.InsertStation("SUDBURY");
+            sm.InsertStation("CLEMSFORD");
+            sm.InsertStation("SWINDON");
+            sm.InsertStation("LIVERPOOL");
+            sm.InsertStation("BIRMINGHAM");
+            sm.InsertStation("CAMBRIDGE");
+            sm.InsertStation("NORWICH");
+
+            Console.WriteLine("===========");
+
+            // RED LINE ==> CONNECTIONS
+            sm.InsertConnection("MANCHESTER","CLIFTON",Colour.RED);
+            sm.InsertConnection("CLIFTON","SHEFFIELD", Colour.RED);
+            sm.InsertConnection("SHEFFIELD","LONDON", Colour.RED);
+            sm.InsertConnection("LONDON","OSTERLY", Colour.RED);
+            sm.InsertConnection("OSTERLY","HEATHROW",Colour.RED);
+            sm.InsertConnection("HEATHROW", "READING", Colour.RED);
+            sm.InsertConnection("READING", "SOUTHAMPTON", Colour.RED);
+            sm.InsertConnection("SOUTHAMPTON", "PORTSMOUTH", Colour.RED);
+            sm.InsertConnection("PORTSMOUTH", "BRIGHTON", Colour.RED);
+
+            // GREEN LINE  ==> CONNECTIONS
+            sm.InsertConnection("LIVERPOOL","BIRMINGHAM",Colour.GREEN);
+            sm.InsertConnection("BIRMINGHAM","LONDON", Colour.GREEN);
+            sm.InsertConnection("LONDON","CAMBRIDGE", Colour.GREEN);
+            sm.InsertConnection("CAMBRIDGE","COLCHESTER", Colour.GREEN);
+            sm.InsertConnection("COLCHESTER","IPSWICH", Colour.GREEN);
+            sm.InsertConnection("IPSWICH","NORWICH", Colour.GREEN);
+
+            // BLUE LINE ==> CONNECTIONS
+            sm.InsertConnection("EDINBURGH", "LEEDS", Colour.BLUE);
+            sm.InsertConnection("LEEDS","LONDON", Colour.BLUE);
+            sm.InsertConnection("LONDON","OXFORD", Colour.BLUE);
+            sm.InsertConnection("OXFORD","NEWBURY", Colour.BLUE);
+            sm.InsertConnection("NEWBURY","WINCHESTER", Colour.BLUE);
+            sm.InsertConnection( "WINCHESTER","MALBOROUGH", Colour.BLUE);
+            sm.InsertConnection("MALBOROUGH","WARMINISTER", Colour.BLUE);
+            sm.InsertConnection("WARMINISTER","BOURNEMOUTH", Colour.BLUE);
+
+            // YELLOW LINE ==> CONNECTIONS
+            sm.InsertConnection("HULL","COLCHESTER",Colour.YELLOW);
+            sm.InsertConnection("COLCHESTER","OSTERLY", Colour.YELLOW);
+            sm.InsertConnection("OSTERLY","HEATHROW", Colour.YELLOW);
+            sm.InsertConnection("HEATHROW","READING", Colour.YELLOW);
+            sm.InsertConnection("READING","WINCHESTER", Colour.YELLOW);
+            sm.InsertConnection("WINCHESTER","BATH", Colour.YELLOW);
+            sm.InsertConnection( "BATH","BRISTOL", Colour.YELLOW);
+            sm.InsertConnection("BRISTOL","CARDIFF", Colour.YELLOW);
+
+
+            //GREY LINE ==> CONNECTIONS
+            sm.InsertConnection("PETERBOROUGH","IPSWICH",Colour.GREY);
+            sm.InsertConnection( "IPSWICH","SUDBURY", Colour.GREY);
+            sm.InsertConnection("SUDBURY","CLEMSFORD",Colour.GREY);
+            sm.InsertConnection("CLEMSFORD", "HEATHROW", Colour.GREY);
+            sm.InsertConnection("HEATHROW","NEWBURY", Colour.GREY);
+            sm.InsertConnection( "NEWBURY","SWINDON", Colour.GREY);
+            sm.InsertConnection("SWINDON", "BATH", Colour.GREY);
+            sm.InsertConnection("BATH", "BRISTOL", Colour.GREY);
+            sm.InsertConnection("BRISTOL", "CARDIFF", Colour.GREY);
+
+            // Test Case 1
+            Console.WriteLine("===========");
+
+            Console.WriteLine("Finding the route from 'Liverpool' to 'Newbury'");
+            sm.SRoute("LIVERPOOL", "NEWBURY");
+
+            // Test Case 2
+            Console.WriteLine("===========");
+
+            Console.WriteLine("Finding the route from 'Cardiff' to 'Ipswich'");
+            sm.SRoute("CARDIFF","IPSWICH");
+
+            // Test Case 3
+            Console.WriteLine("===========");
+
+            //sm.RemoveStation("LONDON");
+            //Console.WriteLine("Finding the route from 'Cardiff' to 'Leeds'");
+            //sm.SRoute("CARDIFF", "LEEDS");
+
+            
+
+            // Test Case 4
+            Console.WriteLine("===========");
+
+            Console.WriteLine("Removing conection from 'Manchester' and 'Clifton'");
+            bool remove1 = sm.RemoveConnection("MANCHESTER", "CLIFTON", Colour.RED);
+            if (remove1 == false)
+            {
+                Console.WriteLine("No connection Found");
+            }
+            else
+            {
+                Console.WriteLine("Connection Removed");
+            }
+
+            Console.WriteLine("Finding the route from 'Manchester' to 'Ipswich'");
+            sm.SRoute("MANCHESTER", "IPSWICH");
+            Console.WriteLine("===========");
+
+            //Test Case 5
+            Console.WriteLine("Finding the route from 'London' to 'Peterborough’");
+            sm.SRoute("LONDON", "PETERBOROUGH");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ q2Maze1.cs
@@ -0,0 +1,227 @@
+﻿//made by 
+//Amber Ahmed #0680481
+//Dharam Raj Patel #0719581
+//Rocky sharaf 
+
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Maze
+{
+    // 2D array to store the maze
+    private char[,] M;
+
+    // Size of the maze
+    private int size;
+
+    // Dimension of the maze (n x n)
+    private int n;
+
+    // Constructor that takes in the dimension of the maze
+    public Maze(int n)
+    {
+        // Store the dimension in the instance variable
+        this.n = n;
+
+        // Call the Initialize method
+        Initialize();
+
+        // Call the Create method to build the maze
+        Create();
+    }
+
+    private void Initialize()
+    {
+        // Calculate the size of the maze
+        size = 2 * n + 3;
+
+        // Create the 2D array to store the maze
+        M = new char[size, size];
+
+        // Loop through the first half of the size of the maze
+        for (int i = 0; i < size / 2; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                // If j is odd, fill with vertical wall ('|')
+                if (j % 2 != 0)
+                {
+                    M[i, j] = '|';
+                }
+                // If i is odd, fill with horizontal wall ('-')
+                else if (i % 2 != 0)
+                {
+                    M[i, j] = '-';
+                }
+                // If i equals the n-th row, fill with horizontal wall ('-')
+                else if ((i == n) & (M[(n), j] != '-'))
+                {
+                    M[(n), j] = '-';
+                }
+                // Fill with space otherwise
+                else
+                {
+                    M[i, j] = ' ';
+                }
+            }
+        }
+    }
+
+
+
+
+
+    private void Create()
+    {
+        // Initialize visited array with size of the matrix
+        bool[,] visited = new bool[size, size];
+
+        // Mark cells that are not inside the matrix border as unvisited
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (j % 2 == 0 && i % 2 == 0 && i != 0 && i != size - 1 && j != 0 && j != size - 1)
+                {
+                    visited[i, j] = false;
+                }
+                else
+                {
+                    visited[i, j] = true;
+                }
+            }
+        }
+
+        // Iterate through each cell of the matrix and perform DFS if the cell is unvisited
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (!visited[i, j])
+                {
+                    DepthFirstSearch(i, j, visited);
+                }
+            }
+        }
+    }
+
+
+
+    // This method implements the Depth First Search algorithm on a 2D grid
+    // to generate a random maze.
+    private void DepthFirstSearch(int i, int j, bool[,] visited)
+    {
+        // Mark the current cell as visited
+        visited[i, j] = true;
+
+        // Create a random number generator
+        Random r = new Random();
+
+        // Store the current position
+        int a = i;
+        int b = j;
+
+        // Create a list to store the possible directions
+        List<char> directions = new List<char>();
+
+        // If the cell to the right is within bounds and unvisited, add direction 'A' to the list
+        if (j + 2 < size && !visited[i, j + 2])
+        {
+            directions.Add('A');
+        }
+
+        // If the cell to the left is within bounds and unvisited, add direction 'B' to the list
+        if (j - 2 >= 0 && !visited[i, j - 2])
+        {
+            directions.Add('B');
+        }
+
+        // If the cell above is within bounds and unvisited, add direction 'C' to the list
+        if (i - 2 >= 0 && !visited[i - 2, j])
+        {
+            directions.Add('C');
+        }
+
+        // If the cell below is within bounds and unvisited, add direction 'D' to the list
+        if (i + 2 < size && !visited[i + 2, j])
+        {
+            directions.Add('D');
+        }
+
+        // If there are any possible directions, choose a random direction
+        if (directions.Count > 0)
+        {
+            char dir = directions[r.Next(directions.Count)];
+
+            // Move to the chosen direction and remove the wall in between
+            if (dir == 'A')
+            {
+                M[i, j + 1] = ' ';
+                b = j + 2;
+            }
+            else if (dir == 'B')
+            {
+                M[i, j - 1] = ' ';
+                b = j - 2;
+            }
+            else if (dir == 'C')
+            {
+                M[i - 1, j] = ' ';
+                a = i - 2;
+            }
+            else if (dir == 'D')
+            {
+                M[i + 1, j] = ' ';
+                a = i + 2;
+            }
+
+            // Recursively call the Depth First Search algorithm from the new position
+            DepthFirstSearch(a, b, visited);
+        }
+    }
+
+
+
+    // Method to print the maze
+    public void Print()
+    {
+        // Loop through all the rows in the maze (excluding the first and last row)
+        for (int i = 1; i < size - 1; i++)
+        {
+            // Loop through all the columns in the current row (excluding the first and last column)
+            for (int j = 1; j < size - 1; j++)
+            {
+                // Print the value of the current cell
+                Console.Write(M[i, j]);
+            }
+            // Move to the next line after printing the current row
+            Console.WriteLine();
+        }
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        // Test for n = 0, 1, 2, 5, 10, 20
+        int[] testCases = new int[] { 0, 1, 2, 5, 10, 20 };
+
+        foreach (int n in testCases)
+        {
+            Console.WriteLine("Maze for n = " + n);
+
+            // Create a Maze object with the given n
+            Maze maze = new Maze(n);
+
+            // Print the maze
+            maze.Print();
+
+            Console.WriteLine();
+        }
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+
+}
